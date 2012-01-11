1a218aebaf173534e863a3dd700ffadd3abfbbe4
Niall Kennedy <niall@niallkennedy.com>
2012-01-10 23:31:36 -0800

    image as structured property

--- html/index.php
+++ html/index.php
@@ -26,6 +26,9 @@ $html = Markdown($markdown);
     <meta property="og:type" content="website">
     <meta property="og:url" content="http://ogp.me/">
     <meta property="og:image" content="http://ogp.me/logo.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="300">
+    <meta property="og:image:height" content="300">
     <meta property="og:description" content="The Open Graph protocol enables any web page to become a rich object in a social graph.">
     <meta prefix="fb: http://ogp.me/ns/fb#" property="fb:app_id" content="115190258555800">
     <link rel="alternate" type="application/rdf+xml" href="http://ogp.me/ns/ogp.me.rdf">
